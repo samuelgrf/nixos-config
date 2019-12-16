a82e7e535cada860721b79c99b8ffd2c4e71e2d5
Samuel Gräfenstein <git@samuelgrf.com>
2019-12-16 19:02:17 +0100

    Move more settings to HP_Pavilion_x360.nix

--- HP_Pavilion_x360.nix
+++ HP_Pavilion_x360.nix
@@ -1,3 +1,17 @@
+{ config, pkgs, ... }:
+
 {
+  # The 32-bit host ID of this machine, formatted as 8 hexadecimal characters.
+  # generated via "head -c 8 /etc/machine-id"
+  # this is required by ZFS
+  networking.hostId = "52623593";
+
+  # Install wifi kernel module
+  boot.extraModulePackages = with pkgs; [ pkgs.linuxPackages.rtl8821ce ];
+
+  # Blacklist sensor kernel modules
+  boot.blacklistedKernelModules = [ "intel_ishtp_hid" "intel_ish_ipc" ];
+
+  # Blacklist power management for wifi card as it may cause issues
   services.tlp.extraConfig = "RUNTIME_PM_BLACKLIST='02:00.0'";
 }
--- configuration.nix
+++ configuration.nix
@@ -126,24 +126,11 @@
   # required for ZFS encryption
   boot.zfs.enableUnstable = true;
 
-  # Request ZFS decryption password on boot
+  # Request ZFS decryption key on boot
   boot.zfs.requestEncryptionCredentials = true;
-
-  # The 32-bit host ID of the machine, formatted as 8 hexadecimal characters.
-  # generated via "head -c 8 /etc/machine-id"
-  # required by ZFS
-  networking.hostId = "52623593";
 
   # Enable NetworkManager
   networking.networkmanager.enable = true;
-
-  # Install wifi kernel module
-  boot.extraModulePackages = with pkgs; [
-    linuxPackages.rtl8821ce
-  ];
-
-  # Blacklist sensor kernel modules
-  boot.blacklistedKernelModules = ["intel_ishtp_hid" "intel_ish_ipc"];
 
   # Install ADB and fastboot
   programs.adb.enable = true;
