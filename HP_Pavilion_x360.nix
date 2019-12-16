ab945bf55307d3659ebf87fd76d11bb35463d76e
Samuel Gräfenstein <git@samuelgrf.com>
2019-12-16 22:38:35 +0100

    Fix audio crackling on Pavilion x360

--- HP_Pavilion_x360.nix
+++ HP_Pavilion_x360.nix
@@ -14,4 +14,16 @@
 
   # Blacklist power management for wifi card as it may cause issues
   services.tlp.extraConfig = "RUNTIME_PM_BLACKLIST='02:00.0'";
+
+  # Install alsa-tools because it is needed to fix audio crackling
+  environment.systemPackages = with pkgs; [ alsaTools ];
+
+  # Create a systemd service to fix audio crackling on startup/resume
+  systemd.services.fixaudio = {
+    description = "Fix audio crackling on Realtek ALC295";
+    script = "${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_COEF_INDEX 0x67\
+              ${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_PROC_COEF 0x3000";
+    wantedBy = [ "multi-user.target" "post-resume.target" ];
+    after = [ "post-resume.target" ];
+  };
 }
