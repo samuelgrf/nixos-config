4e93d94494a4d6bf786c984c46805da063882fb3
Samuel Gräfenstein <git@samuelgrf.com>
2020-02-21 09:51:04 +0100

    [HPx] Refactor fixaudio service

--- hosts/HPx/configuration.nix
+++ hosts/HPx/configuration.nix
@@ -58,8 +58,10 @@
   # Create a systemd service to fix audio crackling on startup/resume
   systemd.services.fixaudio = {
     description = "Fix audio crackling on Realtek ALC295";
-    script = "${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_COEF_INDEX 0x67
-              ${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_PROC_COEF 0x3000";
+    script = ''
+      ${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_COEF_INDEX 0x67
+      ${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_PROC_COEF 0x3000
+    '';
     wantedBy = [ "multi-user.target" "post-resume.target" ];
     after = [ "post-resume.target" ];
   };
