83e506efcc8e822e379df09ea76e2228102b8e6d
Samuel Gräfenstein <git@samuelgrf.com>
2020-05-20 02:27:25 +0200

    Update amdvlk overlay

--- overlays/amdvlk/default.nix
+++ overlays/amdvlk/default.nix
@@ -1,4 +1,4 @@
- { stdenv
+{ stdenv
 , lib
 , fetchpatch
 , fetchRepoProject
@@ -21,17 +21,13 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "amdvlk";
-  version = "2020.Q2.1";
+  version = "2020.Q2.3";
 
   src = fetchRepoProject {
     name = "${pname}-src";
-    # We need to use our own manifest to pin AMDVLK to a specific hash,
-    # it references master in the upstream version.
-    manifest = "https://gist.github.com/98dadf46125c3581a6660fd6040ca7a4.git";
-    # The original manifest is here:
-    #manifest = "https://github.com/GPUOpen-Drivers/AMDVLK.git";
+    manifest = "https://github.com/GPUOpen-Drivers/AMDVLK.git";
     rev = "refs/tags/v-${version}";
-    sha256 = "0g9ja0yyq24s1f1ia721m6lhhl88gv8a930sh4xn8wifwy4vh7c7";
+    sha256 = "00rqr93kq00davf504z072n2cpd68mrlbqkc638l6vx7w77n3di0";
   };
 
   buildInputs = [
@@ -68,22 +64,13 @@ stdenv.mkDerivation rec {
 
   cmakeDir = "../drivers/xgl";
 
-  prePatch = ''
-    sed -i '/-Werror/d' drivers/pal/shared/gpuopen/cmake/AMD.cmake
-  '';
-
-  cmakeFlags = [
-    "-DBUILD_WAYLAND_SUPPORT=ON"
-  ];
-
   installPhase = ''
     mkdir -p $out/lib $out/share/vulkan/icd.d
     cp icd/amdvlk64.so $out/lib/
     cp ../drivers/AMDVLK/json/Redhat/amd_icd64.json $out/share/vulkan/icd.d/
     substituteInPlace $out/share/vulkan/icd.d/amd_icd64.json --replace \
       '/usr/lib64' "$out/lib"
-    patchelf --set-rpath "$rpath" \
-      $out/lib/amdvlk64.so
+    patchelf --set-rpath "$rpath" $out/lib/amdvlk64.so
   '';
 
   # Keep the rpath, otherwise vulkaninfo and vkcube segfault
