68c2fd2ce1dc3c4753da74f8731fae6915ab8db1
Samuel Gräfenstein <git@samuelgrf.com>
2020-05-20 04:53:40 +0200

    [HPx] Reword fixaudio description and add link to bug report

--- hosts/HPx/configuration.nix
+++ hosts/HPx/configuration.nix
@@ -53,8 +53,9 @@
   ];
 
   # Create a systemd service to fix audio crackling on startup/resume
+  # https://bugs.launchpad.net/ubuntu/+source/alsa-driver/+bug/1648183/comments/31
   systemd.services.fixaudio = {
-    description = "Fix audio crackling on Realtek ALC295";
+    description = "Audio crackling fix for Realtek ALC295";
     script = ''
       ${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_COEF_INDEX 0x67
       ${pkgs.alsaTools}/bin/hda-verb /dev/snd/hwC[[:print:]]*D0 0x20 SET_PROC_COEF 0x3000
