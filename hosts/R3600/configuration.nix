6af44d0046e874af6e9e162683585b1b7d964d74
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-02 16:15:53 +0200

    [R3600] Change boot loader timeout to 3 seconds

--- hosts/R3600/configuration.nix
+++ hosts/R3600/configuration.nix
@@ -3,7 +3,7 @@
 {
   # Configure systemd-boot
   boot.loader.systemd-boot.enable = true;
-  boot.loader.timeout = 1;
+  boot.loader.timeout = 3;
 
   # Set hostname
   networking.hostName = "R3600";
