9cc4514de455c9e81403d1e3d6c15abe05b8d526
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-18 23:53:12 +0200

    Update g810-led overlay and module

--- overlays/g810-led/default.nix
+++ overlays/g810-led/default.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, fetchgit, hidapi }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "g810-led";
-  version = "0.3.9";
+  version = "0.4.2";
 
   src = fetchgit {
     url = "https://github.com/MatMoul/g810-led.git";
-    rev = "v0.3.9";
-    sha256 = "16k5zqr5p1426a33wwfh97s6b45bkj5adb6xpnn88pqc854hnsx7";
+    rev = "v${version}";
+    sha256 = "1ymkp7i7nc1ig2r19wz0pcxfnpawkjkgq7vrz6801xz428cqwmhl";
   };
 
   buildInputs = [ hidapi ];
@@ -20,14 +20,15 @@ stdenv.mkDerivation {
     ln -s $out/bin/g810-led $out/bin/g512-led
     ln -s $out/bin/g810-led $out/bin/g513-led
     ln -s $out/bin/g810-led $out/bin/g610-led
+    ln -s $out/bin/g810-led $out/bin/g815-led
     ln -s $out/bin/g810-led $out/bin/g910-led
     ln -s $out/bin/g810-led $out/bin/gpro-led
   '';
 
   meta = with stdenv.lib; {
-    description = "Linux led controller for Logitech G213, G410, G413, G512, G513, G610, G810, G910 and GPRO Keyboards";
+    description = "Linux led controller for Logitech G213, G410, G413, G512, G513, G610, G810, G815, G910 and GPRO Keyboards";
     homepage = "https://github.com/MatMoul/g810-led";
-    maintainers = with maintainers; [ samuelgrf ];
+    # maintainers = with maintainers; [ samuelgrf ];
     license = licenses.gpl3;
     platforms = stdenv.lib.platforms.linux;
   };
