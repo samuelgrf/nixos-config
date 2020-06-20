b7f5035f35bd6c33ebf9ec7e61afaadda62397d7
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-20 22:58:44 +0200

    Sort printers alphabetically

--- common.nix
+++ common.nix
@@ -188,28 +188,13 @@ in
   # Add printers
   hardware.printers.ensurePrinters = [
     {
-      name = "HP_OfficeJet_Pro_7720";
-      description = "HP Officejet Pro 7720";
-      location = "Office Downstairs";
-      deviceUri = "hp:/net/OfficeJet_Pro_7720_series?ip=192.168.178.36";
-      # Get installed PPD files by running "lpinfo -m"
-      model = "drv:///hp/hpcups.drv/hp-officejet_pro_7720_series.ppd";
-      # Get options by running "lpoptions -p HP_OfficeJet_Pro_7720 -l"
-      ppdOptions = {
-        "PageSize" = "A3";
-        "ColorModel" = "RGB";
-        "MediaType" = "Plain";
-        "OutputMode" = "Normal"; # Quality, can be "Normal", "FastDraft", "Best" or "Photo"
-        "InputSlot" = "Upper"; # Scanning?
-        "Duplex" = "None";
-      };
-    }
-    {
       name = "Brother_MFC-L2700DW";
       description = "Brother MFC-L2700DW";
       location = "Office Upstairs";
       deviceUri = "ipp://192.168.178.37/ipp";
+      # Get installed PPD files by running "lpinfo -m"
       model = "brother-MFCL2700DN-cups-en.ppd";
+      # Get options by running "lpoptions -p Brother_MFC-L2700DW -l"
       ppdOptions = {
         "PageSize" = "A4";
         "BrMediaType" = "PLAIN";
@@ -220,6 +205,21 @@ in
         # Timeout before going to sleep after printing.
         # Can be "PrinterDefault", "2minutes", "10minutes" or "30minutes"
         "Sleep" = "PrinterDefault";
+      };
+    }
+    {
+      name = "HP_OfficeJet_Pro_7720";
+      description = "HP Officejet Pro 7720";
+      location = "Office Downstairs";
+      deviceUri = "hp:/net/OfficeJet_Pro_7720_series?ip=192.168.178.36";
+      model = "drv:///hp/hpcups.drv/hp-officejet_pro_7720_series.ppd";
+      ppdOptions = {
+        "PageSize" = "A3";
+        "ColorModel" = "RGB";
+        "MediaType" = "Plain";
+        "OutputMode" = "Normal"; # Quality, can be "Normal", "FastDraft", "Best" or "Photo"
+        "InputSlot" = "Upper"; # Scanning?
+        "Duplex" = "None";
       };
     }
   ];
