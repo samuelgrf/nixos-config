f2bead262ef3127391fbd64055e4210b1135a213
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-27 18:07:35 +0200

    Overlays: Add amdvlk_noDefault overlay
    The Vulkan Loader tries to load the default driver from "$share/vulkan/icd.d/".
    Prevent loading AMDVLK by default by creating a symlink to "$share/vulkan/icd.d"
    and moving it to "$share/amdvlk".

--- overlays/default.nix
+++ overlays/default.nix
@@ -77,6 +77,17 @@
       ## Misc
       ##########################################################################
 
+      # The Vulkan Loader tries to load the default driver from $share/vulkan/icd.d/
+      # Prevent loading AMDVLK by default by moving the driver to $share/amdvlk/icd.d/
+      amdvlk_noDefault = super.pkgs.symlinkJoin {
+        name = "amdvlk_noDefault";
+        paths = [ "${super.pkgs.amdvlk}/share/vulkan" ];
+        postBuild = ''
+          mkdir -p $out/share/amdvlk
+          mv $out/icd.d $out/share/amdvlk
+        '';
+      };
+
       mpv_sponsorblock = super.pkgs.mpv.override {
         scripts = [
           (super.callPackage ./mpv-scripts/sponsorblock.nix { })
