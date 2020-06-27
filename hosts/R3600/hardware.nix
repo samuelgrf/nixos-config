52c12576576effeb707dd433b9f3e2f16dc5d0f5
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-27 16:32:16 +0200

    HPx & R3600: Get hardware.nix imports from modulesPath
    To be consistent with hardware-config.nix generated by running
    "nixos-generate-config"

--- hosts/R3600/hardware.nix
+++ hosts/R3600/hardware.nix
@@ -1,8 +1,8 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
   imports =
-    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "amdgpu" ];
