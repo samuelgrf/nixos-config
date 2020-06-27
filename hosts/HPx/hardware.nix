352cb7ee7f084d767d4985281b11c4c676e1a0c9
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-27 15:11:48 +0200

    HPx & R3600: Refactor hardware.nix
    To be consistent with hardware-config.nix generated by running
    "nixos-generate-config"

--- hosts/HPx/hardware.nix
+++ hosts/HPx/hardware.nix
@@ -1,42 +1,40 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-  ];
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  boot.initrd.luks.devices = {
-    luksroot = {
-      device = "/dev/disk/by-uuid/97e06f70-899d-44cf-baff-d09e5a2daa59";
+  boot.initrd.luks.devices.luksroot =
+    { device = "/dev/disk/by-uuid/97e06f70-899d-44cf-baff-d09e5a2daa59";
       allowDiscards = true;
     };
-  };
 
-  fileSystems."/" = {
-    device = "rpool/root/nixos";
-    fsType = "zfs";
-  };
+  fileSystems."/" =
+    { device = "rpool/root/nixos";
+      fsType = "zfs";
+    };
 
-  fileSystems."/home" = {
-    device = "rpool/home";
-    fsType = "zfs";
-  };
+  fileSystems."/home" =
+    { device = "rpool/home";
+      fsType = "zfs";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/E7F8-95F0";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/E7F8-95F0";
+      fsType = "vfat";
+    };
 
-  swapDevices = [
-    { device = "/dev/disk/by-partuuid/c4114437-3b41-41c3-b005-ce9f8171815f";
-      randomEncryption.enable = true;
-    }
-  ];
+  swapDevices =
+    [ { device = "/dev/disk/by-partuuid/c4114437-3b41-41c3-b005-ce9f8171815f";
+        randomEncryption.enable = true;
+      }
+    ];
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
