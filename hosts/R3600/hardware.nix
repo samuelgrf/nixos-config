382cb9102cfec4442814c1dcec975fbd4cd906b3
Samuel Gräfenstein <git@samuelgrf.com>
2020-06-27 17:29:00 +0200

    R3600: Remove unneeded initrd modules
    amdgpu causes the decryption prompt to show up later and makes text
    very small on High-DPI screens.
    usb_storage isn't needed for booting into NixOS.

--- hosts/R3600/hardware.nix
+++ hosts/R3600/hardware.nix
@@ -5,7 +5,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "amdgpu" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
