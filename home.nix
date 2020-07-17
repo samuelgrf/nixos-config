13697ad6563e4964149e2deba4c602c28af3703a
Samuel Gräfenstein <git@samuelgrf.com>
2020-07-17 10:04:17 +0200

    home: Reword stateVersion comment

--- home.nix
+++ home.nix
@@ -4,14 +4,8 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
+  # The NixOS release to be compatible with for stateful data such as databases.
+  # Read the release notes before changing this.
   home.stateVersion = "20.03";
 
   # Needed to get the GPG password dialog to work
