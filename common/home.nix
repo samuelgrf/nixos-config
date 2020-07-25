f4b5252925759c15bc0f3592cba54e3b3109f651
Samuel Gräfenstein <git@samuelgrf.com>
2020-07-25 17:07:46 +0200

    home: Remove Wine default associations
    Wine thinks it's an amazing idea to generate lots of .desktop files
    everytime a new prefix is created. These make the last Wine executable
    that was ran (including those from Steam and Lutris) the default
    for multiple filetypes.

--- common/home.nix
+++ common/home.nix
@@ -52,5 +52,34 @@
       ${xhtml} = firefox;
       ${xml} = kate;
     };
+
+    # Wine thinks it's an amazing idea to generate lots of .desktop files
+    # everytime a new prefix is created. These make the last Wine executable
+    # that was ran (including those from Steam and Lutris) the default
+    # for multiple filetypes.
+    associations.removed = {
+      "application/pdf" = "wine-extension-pdf.desktop";
+      "application/rtf" = "wine-extension-rtf.desktop";
+      "application/vnd.ms-htmlhelp" = "wine-extension-chm.desktop";
+      "application/winhlp" = "wine-extension-hlp.desktop";
+      "application/x-ms-application" = "wine-extension-application.desktop";
+      "application/x-ms-xbap" = "wine-extension-xbap.desktop";
+      "application/x-mswinurl" = "wine-extension-url.desktop";
+      "application/x-mswrite" = "wine-extension-wri.desktop";
+      "application/x-wine-extension-appref-ms" = "wine-extension-appref-ms.desktop";
+      "application/x-wine-extension-compositefont" = "wine-extension-compositefont.desktop";
+      "application/x-wine-extension-crd" = "wine-extension-crd.desktop";
+      "application/x-wine-extension-crds" = "wine-extension-crds.desktop";
+      "application/x-wine-extension-ini" = "wine-extension-ini.desktop";
+      "application/x-wine-extension-msp" = "wine-extension-msp.desktop";
+      "application/x-wine-extension-vbs" = "wine-extension-vbs.desktop";
+      "application/xaml+xml" = "wine-extension-xaml.desktop";
+      "application/xml" = "wine-extension-xml.desktop";
+      "image/gif" = "wine-extension-gif.desktop";
+      "image/jpeg" = [ "wine-extension-jfif.desktop" "wine-extension-jpe.desktop" ];
+      "image/png" = "wine-extension-png.desktop";
+      "text/html" = "wine-extension-htm.desktop";
+      "text/plain" = "wine-extension-txt.desktop";
+    };
   };
 }
