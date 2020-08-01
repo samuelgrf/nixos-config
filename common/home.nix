d91532920c358107a02e468353d2e031fade99a5
Samuel Gräfenstein <git@samuelgrf.com>
2020-08-01 18:08:41 +0200

    home: Refactor

--- common/home.nix
+++ common/home.nix
@@ -26,16 +26,16 @@
   xdg.mimeApps = let
 
     # Applications
-    kate = "org.kde.kate.desktop";
     firefox = "firefox.desktop";
     gwenview = "org.kde.gwenview.desktop";
+    kate = "org.kde.kate.desktop";
     okularPdf = "okularApplication_pdf.desktop";
 
     # Formats
     gif = "image/gif";
+    html = "text/html";
     http = "x-scheme-handler/http";
     https = "x-scheme-handler/https";
-    html = "text/html";
     ico = "image/x-ico";
     ini = "application/x-wine-extension-ini";
     jpg = "image/jpeg";
@@ -44,15 +44,15 @@
     svg = "image/svg+xml";
     txt = "text/plain";
     url = "application/x-mswinurl";
-    xml = "application/xml";
     xhtml = "application/xhtml+xml";
+    xml = "application/xml";
+
   in {
     enable = true;
-
     associations.added = {
       ${ini} = kate;
-      ${url} = kate;
       ${svg} = gwenview;
+      ${url} = kate;
       ${xml} = kate;
     };
     defaultApplications = {
@@ -77,28 +77,28 @@
     # that was ran (including those from Steam and Lutris) the default
     # for multiple filetypes.
     associations.removed = {
-      "application/pdf" = "wine-extension-pdf.desktop";
       "application/rtf" = "wine-extension-rtf.desktop";
       "application/vnd.ms-htmlhelp" = "wine-extension-chm.desktop";
       "application/winhlp" = "wine-extension-hlp.desktop";
       "application/x-ms-application" = "wine-extension-application.desktop";
       "application/x-ms-xbap" = "wine-extension-xbap.desktop";
-      "application/x-mswinurl" = "wine-extension-url.desktop";
       "application/x-mswrite" = "wine-extension-wri.desktop";
       "application/x-wine-extension-appref-ms" = "wine-extension-appref-ms.desktop";
       "application/x-wine-extension-compositefont" = "wine-extension-compositefont.desktop";
       "application/x-wine-extension-crd" = "wine-extension-crd.desktop";
       "application/x-wine-extension-crds" = "wine-extension-crds.desktop";
-      "application/x-wine-extension-ini" = "wine-extension-ini.desktop";
       "application/x-wine-extension-msp" = "wine-extension-msp.desktop";
       "application/x-wine-extension-vbs" = "wine-extension-vbs.desktop";
       "application/xaml+xml" = "wine-extension-xaml.desktop";
-      "application/xml" = "wine-extension-xml.desktop";
-      "image/gif" = "wine-extension-gif.desktop";
-      "image/jpeg" = [ "wine-extension-jfif.desktop" "wine-extension-jpe.desktop" ];
-      "image/png" = "wine-extension-png.desktop";
-      "text/html" = "wine-extension-htm.desktop";
-      "text/plain" = "wine-extension-txt.desktop";
+      ${gif} = "wine-extension-gif.desktop";
+      ${html} = "wine-extension-htm.desktop";
+      ${ini} = "wine-extension-ini.desktop";
+      ${jpg} = [ "wine-extension-jfif.desktop" "wine-extension-jpe.desktop" ];
+      ${pdf} = "wine-extension-pdf.desktop";
+      ${png} = "wine-extension-png.desktop";
+      ${txt} = "wine-extension-txt.desktop";
+      ${url} = "wine-extension-url.desktop";
+      ${xml} = "wine-extension-xml.desktop";
     };
   };
 }
