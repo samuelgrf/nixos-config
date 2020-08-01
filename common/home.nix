1c65c0248a03fd0fa479351313ee4f014250c518
Samuel Gräfenstein <git@samuelgrf.com>
2020-08-01 15:05:19 +0200

    home: Keep mpv open after playback is finished

--- common/home.nix
+++ common/home.nix
@@ -14,10 +14,13 @@
     signing.signByDefault = true;
   };
 
-  # Prefer VP9 and Opus codecs for youtube-dl streams in mpv.
-  programs.mpv.enable = true;
-  programs.mpv.config.ytdl-format =
-    "(bestvideo[vcodec=vp9]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)/best";
+  # Set mpv configuration.
+  programs.mpv = {
+    enable = true;
+    config.keep-open = true; # Keep mpv open after playback is finished.
+    config.ytdl-format = # Prefer VP9 and Opus codecs for youtube-dl streams.
+      "(bestvideo[vcodec=vp9]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)/best";
+  };
 
   # Set default applications.
   xdg.mimeApps = let
