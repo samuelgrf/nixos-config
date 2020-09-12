30e1974dad83cc4c98dfe18171a03f6d445dc423
Samuel Gräfenstein <git@samuelgrf.com>
2020-09-12 23:58:44 +0200

    overlays: Update g810-led

--- overlays/g810-led/default.nix
+++ overlays/g810-led/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, hidapi
+{ stdenv, fetchFromGitHub, hidapi
 , profile ? "/etc/g810-led/profile"
 }:
 
@@ -6,8 +6,9 @@ stdenv.mkDerivation rec {
   pname = "g810-led";
   version = "0.4.2";
 
-  src = fetchgit {
-    url = "https://github.com/MatMoul/g810-led.git";
+  src = fetchFromGitHub {
+    owner = "MatMoul";
+    repo = pname;
     rev = "v${version}";
     sha256 = "1ymkp7i7nc1ig2r19wz0pcxfnpawkjkgq7vrz6801xz428cqwmhl";
   };
@@ -34,9 +35,9 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Linux LED controller for Logitech G213, G410, G413, G512, G513, G610, G810, G815, G910 and GPRO Keyboards";
-    homepage = "https://github.com/MatMoul/g810-led";
-    maintainers = [ maintainers.samuelgrf ];
+    homepage = src.meta.homepage;
     license = licenses.gpl3;
     platforms = platforms.linux;
+    maintainers = [ maintainers.samuelgrf ];
   };
 }
