678611e2aea380dcae940f19e62b79f4b8faeb2e
Samuel Gräfenstein <git@samuelgrf.com>
2020-12-18 07:39:14 +0100

    kernel: Small refactor

--- common/kernel.nix
+++ common/kernel.nix
@@ -16,5 +16,5 @@
 
   # Set swappiness to 80 due to improved performance of zram.
   # Can be up to 100, but will increase process queue on intense load such as boot.
-  boot.kernel.sysctl = { "vm.swappiness" = 80; };
+  boot.kernel.sysctl."vm.swappiness" = 80;
 }
