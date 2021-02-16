fc7a64a66d7b7a883fa62f026b531583baef3f0c
Samuel Gräfenstein <git@samuelgrf.com>
2021-02-16 22:29:41 +0100

    home: Small refactor

--- home/default.nix
+++ home/default.nix
@@ -13,10 +13,10 @@
     signing.key = "FF2458328FAF466018C6186EEF76A063F15C63C8";
     signing.signByDefault = true;
 
-    extraConfig = {
-      core.pager = "delta";
-      delta.line-numbers = true;
+    delta.enable = true;
+    delta.options.line-numbers = true;
 
+    extraConfig = {
       pull.ff = "only";
       url."ssh://git@github.com/".pushInsteadOf = https://github.com/;
       url."ssh://git@gitlab.com/".pushInsteadOf = https://gitlab.com/;
