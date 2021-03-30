f43de5cf6e29a32ea00bf5ce9e57e594d2c4b6b4
Samuel Gräfenstein <git@samuelgrf.com>
2021-03-30 11:16:39 +0200

    home/proton: 6.4-GE-1 -> 6.5-GE-1

--- home/proton.nix
+++ home/proton.nix
@@ -2,11 +2,11 @@
 
   # Install GloriousEggroll's custom Proton build.
   home.file.".local/share/Steam/compatibilitytools.d/Proton-GE".source =
-    let version = "6.4-GE-1";
+    let version = "6.5-GE-1";
     in fetchzip {
       url =
         "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/Proton-${version}.tar.gz";
-      hash = "sha256-vKDTuK3RhqmuTIwB3rDRgY/ki6kLZ/RsfHJszN1ADlM=";
+      hash = "sha256-8rjSLJY83R1PIqMSsCAbkVwS40WYf/aRQtZqW44yGMQ=";
     };
 
 }
