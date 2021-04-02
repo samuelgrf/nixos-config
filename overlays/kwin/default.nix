94b55f4454623e489bd6522e6cadffeb468f5c37
Samuel Gräfenstein <git@samuelgrf.com>
2021-04-02 14:32:12 +0200

    overlays/kwin: Refactor & Add version check

--- overlays/kwin/default.nix
+++ overlays/kwin/default.nix
@@ -1,17 +1,26 @@
 _: prev:
-with prev; {
+with prev;
 
-  # Patch files can be found here: https://tildearrow.org/storage/kwin-lowlatency
+# Patch files can be found here: https://tildearrow.org/storage/kwin-lowlatency
+let
+  version = "5.18.5";
+  revision = "3";
+in {
+
   plasma5 = plasma5 // {
 
     kwin = plasma5.kwin.overrideAttrs (oldAttrs: {
       patches = (oldAttrs.patches or [ ]) ++ [
         (fetchpatch {
           url =
-            "https://tildearrow.org/storage/kwin-lowlatency/kwin-lowlatency-5.18.5-3.patch";
+            "https://tildearrow.org/storage/kwin-lowlatency/kwin-lowlatency-${version}${
+              if revision == "0" then "" else "-${revision}"
+            }.patch";
           sha256 = "sha256-HaHw7CDayhtlTA8qs8maUsz4qjHTVUsYaFg9IFxjGhM=";
         })
       ];
+
+      meta.broken = (lib.getVersion plasma5.kwin) != version;
     });
 
   };
