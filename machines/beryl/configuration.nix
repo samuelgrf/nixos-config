57ba858be1b50e1e6449d5474a9b1d9a44ab482c
Samuel Gräfenstein <git@samuelgrf.com>
2021-04-30 14:46:51 +0200

    beryl: Add TODOs for amdvlk changes on 21.05

--- machines/beryl/configuration.nix
+++ machines/beryl/configuration.nix
@@ -46,11 +46,13 @@
 
   # Install AMDVLK driver, as some games have graphical glitches when using RADV.
   # Can be enabled by setting
+  # TODO Update variable to `AMD_VULKAN_ICD=AMDVLK` on 21.05.
   # `VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/amd_icd64.json:/run/opengl-driver-32/share/vulkan/icd.d/amd_icd32.json`.
   hardware.opengl.extraPackages = [ amdvlk ];
   hardware.opengl.extraPackages32 = with pkgsi686Linux; [ amdvlk ];
 
   # Set global environment variables.
+  # TODO Change to environment.variables.AMD_VULKAN_ICD = "RADV"; on 21.05.
   environment.variables.VK_ICD_FILENAMES =
     "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json";
 
