482fb2d48cc158121ac95757cd607d324ac2f6b4
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-01 08:37:25 +0200

    repl: Load modified lib

--- repl.nix
+++ repl.nix
@@ -1,8 +1,10 @@
 let
   self = __getFlake "self";
   lib = self.inputs.nixpkgs.lib;
+  lib' = lib // import ./lib { inherit lib; };
   host = lib.removeSuffix "\n" (__readFile "/etc/hostname");
   nixosConfig = self.nixosConfigurations.${host};
 in {
-  inherit (nixosConfig._module.args) lib pkgs-master pkgs-unstable;
+  lib = lib';
+  inherit (nixosConfig._module.args) pkgs-master pkgs-unstable;
 } // nixosConfig // self.inputs // self
