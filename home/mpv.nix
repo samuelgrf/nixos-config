6bacf79f811bc59291a86b33eb23bb8f7d74c040
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-03 22:42:01 +0200

    home/mpv: Always start in GUI mode

--- home/mpv.nix
+++ home/mpv.nix
@@ -3,9 +3,12 @@
   # Configure mpv.
   programs.mpv = {
     enable = true;
-    config.keep-open = true; # Keep mpv open after playback is finished.
-    config.ytdl-format = # Prefer VP9 and Opus codecs for youtube-dl streams.
-      "(bestvideo[vcodec=vp9]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)/best";
+    config = {
+      player-operation-mode = "pseudo-gui"; # Always start in GUI mode.
+      keep-open = true; # Keep mpv open after playback is finished.
+      ytdl-format = # Prefer VP9 and Opus codecs for youtube-dl streams.
+        "(bestvideo[vcodec=vp9]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)/best";
+    };
   };
 
 }
