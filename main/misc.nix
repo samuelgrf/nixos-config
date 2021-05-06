4280dc4cd17d882961fd1134b0759abda3fdec3d
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-06 18:15:27 +0200

    misc: Small refactor

--- main/misc.nix
+++ main/misc.nix
@@ -1,10 +1,10 @@
 {
 
-  # Use Qt dialogs in supported GTK applications.
-  xdg.portal.gtkUsePortal = true;
-
   # Wipe /tmp on boot.
   boot.cleanTmpDir = true;
+
+  # Use Qt dialogs in supported GTK applications.
+  xdg.portal.gtkUsePortal = true;
 
   # Show Proton builds from `~/.steam/root/compatibilitytools.d` in Lutris.
   environment.variables.LUTRIS_ENABLE_PROTON = "1";
