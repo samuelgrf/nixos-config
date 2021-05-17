7a6abd806318e605348ca7fb44c32bc606de2d9b
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-17 08:20:26 +0200

    overlays: Small refactor

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,10 @@
 { flakes }: [
 
   # TODO Remove on 21.05.
+  # kdeGear: Alias for packages related to KDE.
+  (_: prev: { kdeGear = prev.kdeApplications // prev.libsForQt5; })
+
+  # TODO Remove on 21.05.
   # libstrangle: Get from nixpkgs unstable.
   (import ./libstrangle { inherit (flakes) nixpkgs-unstable; })
 
@@ -9,10 +13,6 @@
 
   # plasma5.kwin: Apply low latency patch.
   (import ./kwin)
-
-  # TODO Remove on 21.05.
-  # kdeGear: Alias for packages related to KDE.
-  (_: prev: { kdeGear = prev.kdeApplications // prev.libsForQt5; })
 
   # ungoogled-chromium: Add command line arguments.
   (import ./ungoogled-chromium)
