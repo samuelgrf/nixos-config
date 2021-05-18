0a501fb381d1b41589fb0c3da974a433e08d21ad
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-18 23:59:59 +0200

    repl: Fix loading custom lib

--- repl.nix
+++ repl.nix
@@ -1,7 +1,8 @@
 let
   self = __getFlake "self";
+  pkgs = self.inputs.nixpkgs.legacyPackages.x86_64-linux;
   lib = self.inputs.nixpkgs.lib;
-  lib' = lib // import ./lib { inherit lib; };
+  lib' = lib // import ./lib { inherit lib pkgs; };
   host = lib.removeSuffix "\n" (__readFile "/etc/hostname");
   nixosConfig = self.nixosConfigurations.${host};
 in {
