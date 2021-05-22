d541d8539cd4e0b67dedcef7d45b9c2fbc942393
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-22 18:17:09 +0200

    services: Add authorized SSH keys

--- main/services.nix
+++ main/services.nix
@@ -78,6 +78,16 @@
     startAgent = true; # Enable key manager.
     askPassword = ""; # Disable GUI password prompt.
   };
+  users.users = {
+    samuel.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDwTxBMRYCd0AKW5vDWbOuyfevl+VH/ntDwrvFw5rbzt samuel@amethyst"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnGnsUmj08UT8r8nDfStCgDpo0e2KrhTb+69e2QKZvA samuel@beryl"
+    ];
+    root.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC68A6rSbE4UeZgTLJKiIVbTgZRgeeVy8P2BWWgVbWp6 root@amethyst"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeWZ1+Yxu0qcL131/4M3o0++qNgFXANrTxSJe8JbzZa root@beryl"
+    ];
+  };
 
   # Enable GnuPG agent.
   programs.gnupg.agent.enable = true;
