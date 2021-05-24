1924931ffb5f17aaf8d96d997942f8944ebde06c
Samuel Gräfenstein <git@samuelgrf.com>
2021-05-24 22:21:20 +0200

    overlays/nixos-rebuild: Update
    Fix nixos-rebuild switching the system to a preexisting
    `./result` symlink on build failure.

--- overlays/nixos-rebuild/default.nix
+++ overlays/nixos-rebuild/default.nix
@@ -4,8 +4,8 @@ with prev; {
   nixos-rebuild = nixos-rebuild.overrideAttrs (_: {
     src = fetchurl {
       url =
-        "https://raw.githubusercontent.com/samuelgrf/nixpkgs/63a96861b018ad682d4c85745ec7c006d8572f18/pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh";
-      hash = "sha256-nHK1sDQ17Viljb/4OEMhp8lcElg8tGb4OGfO935jRGs=";
+        "https://raw.githubusercontent.com/samuelgrf/nixpkgs/4d60a32db03215ef2f986f1f29f0fe0b9ba40032/pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh";
+      hash = "sha256-VKWVKo2D4j0HdMmq4y3TPLSCOGAfcFtZqALt6dXEp9I=";
     };
   });
 
