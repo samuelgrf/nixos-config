1b2fd5a092d97788790abfc65fb8c77a3b98cba5
Samuel Gräfenstein <git@samuelgrf.com>
2021-06-03 20:00:07 +0200

    chromium: Reorder bookmarks

--- main/chromium.nix
+++ main/chromium.nix
@@ -59,56 +59,6 @@
       ManagedBookmarks = let release = config.system.nixos.release;
       in [
         {
-          name = "Nix & NixOS";
-          children = [
-            {
-              name = "NixOS/nixpkgs: Nix Packages collection";
-              url = "https://github.com/NixOS/nixpkgs";
-            }
-            {
-              name = "NixOS Infra Status";
-              url = "https://status.nixos.org/";
-            }
-            {
-              name = "Nixpkgs PR progress tracker";
-              url = "https://nixpk.gs/pr-tracker.html";
-            }
-            {
-              name = "Useful Nix Hacks";
-              url = "http://chriswarbo.net/projects/nixos/useful_hacks.html";
-            }
-            {
-              name = "Hydra - Overview";
-              url = "https://hydra.nixos.org/";
-            }
-            {
-              name = "Hydra - Jobset nixos:release-${release}";
-              url = "https://hydra.nixos.org/jobset/nixos/release-${release}";
-            }
-            {
-              name = "Hydra - Jobset nixos:trunk-combined";
-              url = "https://hydra.nixos.org/jobset/nixos/trunk-combined";
-            }
-          ];
-        }
-        {
-          name = "Chrome";
-          children = [
-            {
-              name = "Chrome Release Schedule";
-              url = "https://www.chromestatus.com/features/schedule";
-            }
-            {
-              name = "Chrome Enterprise release notes";
-              url = "https://support.google.com/chrome/a/answer/7679408/";
-            }
-            {
-              name = "Chrome Enterprise policy list";
-              url = "https://chromeenterprise.google/policies/";
-            }
-          ];
-        }
-        {
           name = "Manuals";
           children = [
             {
@@ -129,6 +79,56 @@
               name = "Home Manager Manual";
               url =
                 "file:///run/current-system/sw/share/doc/home-manager/index.html";
+            }
+          ];
+        }
+        {
+          name = "Nix & NixOS";
+          children = [
+            {
+              name = "NixOS/nixpkgs: Nix Packages collection";
+              url = "https://github.com/NixOS/nixpkgs";
+            }
+            {
+              name = "Nixpkgs PR progress tracker";
+              url = "https://nixpk.gs/pr-tracker.html";
+            }
+            {
+              name = "NixOS Infra Status";
+              url = "https://status.nixos.org/";
+            }
+            {
+              name = "Hydra - Overview";
+              url = "https://hydra.nixos.org/";
+            }
+            {
+              name = "Hydra - Jobset nixos:release-${release}";
+              url = "https://hydra.nixos.org/jobset/nixos/release-${release}";
+            }
+            {
+              name = "Hydra - Jobset nixos:trunk-combined";
+              url = "https://hydra.nixos.org/jobset/nixos/trunk-combined";
+            }
+            {
+              name = "Useful Nix Hacks";
+              url = "http://chriswarbo.net/projects/nixos/useful_hacks.html";
+            }
+          ];
+        }
+        {
+          name = "Chrome";
+          children = [
+            {
+              name = "Chrome Release Schedule";
+              url = "https://www.chromestatus.com/features/schedule";
+            }
+            {
+              name = "Chrome Enterprise release notes";
+              url = "https://support.google.com/chrome/a/answer/7679408/";
+            }
+            {
+              name = "Chrome Enterprise policy list";
+              url = "https://chromeenterprise.google/policies/";
             }
           ];
         }
