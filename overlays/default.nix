cd7d4c3980f51858a647e9ab023f4ecdc249fae7
Samuel Gräfenstein <git@samuelgrf.com>
2021-06-03 17:42:06 +0200

    overlays/nix: Install Zsh completion script

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,9 @@
 [
 
+  # TODO Remove once `nixUnstable` is updated in nixpkgs.
+  # nix: Install Zsh completion script.
+  (import ./nix)
+
   # TODO Remove once https://github.com/NixOS/nixpkgs/pull/123720 is merged.
   # nixos-rebuild: Fix creating ./result symlink for flakes.
   (import ./nixos-rebuild)
