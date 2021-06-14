f85f92587f970fa97305f854996057db43f8d16b
Samuel Gräfenstein <git@samuelgrf.com>
2021-06-14 15:55:18 +0200

    Revert "misc: Show Proton builds in Lutris"
    This reverts commit a162bc56c15e8a699cbf1c67bc37100f420b4146.

--- main/misc.nix
+++ main/misc.nix
@@ -6,7 +6,4 @@
   # Use Qt dialogs in supported GTK applications.
   xdg.portal.gtkUsePortal = true;
 
-  # Show Proton builds from `~/.steam/root/compatibilitytools.d` in Lutris.
-  environment.variables.LUTRIS_ENABLE_PROTON = "1";
-
 }
