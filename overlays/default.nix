451ee4a754b0f7bedea88569e1cb233cb8e2a2ca
Samuel Gräfenstein <git@samuelgrf.com>
2021-06-18 22:10:48 +0200

    overlays/nix: Remove

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,10 +3,6 @@
   # gimpPlugins.bimp: Init
   (import ./gimpPlugins)
 
-  # TODO Remove once `nixUnstable` is updated in nixpkgs.
-  # nix: Install Zsh completion script.
-  (import ./nix)
-
   # TODO Remove once https://github.com/NixOS/nixpkgs/pull/123720 is merged.
   # nixos-rebuild: Fix creating ./result symlink for flakes.
   (import ./nixos-rebuild)
