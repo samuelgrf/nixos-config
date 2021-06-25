91f8349f767f3d9216af926a533cec2b625f589d
Samuel Gräfenstein <git@samuelgrf.com>
2021-06-25 22:38:58 +0200

    kernel: Don't use unstable ZFS package

--- main/kernel.nix
+++ main/kernel.nix
@@ -4,9 +4,6 @@
   # Linux kernel 5.12 supports DualSense and RTL8821CE.
   boot.kernelPackages = linuxPackages_5_12;
 
-  # TODO Remove once OpenZFS is updated to 2.1.0.
-  boot.zfs.enableUnstable = true; # Supports Linux 5.12.
-
   # Enable support for additional filesystems.
   boot.supportedFilesystems = [ "exfat" "ntfs" "zfs" ];
   networking.hostId = lib.mkHostId config.networking.hostName; # Needed by ZFS.
