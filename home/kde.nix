97a85d7effd270b5de05f7073ea818f42ebfaba2
Samuel Gräfenstein <git@samuelgrf.com>
2021-06-30 22:07:06 +0200

    home/kde: Set power management profiles

--- home/kde.nix
+++ home/kde.nix
@@ -123,6 +123,28 @@
         Data_4Triggers0.Uuid = "{f3242e9b-1c88-455f-be32-f9d50105551a}";
       };
 
+      # Set power management profiles.
+      powermanagementprofilesrc = {
+        AC = {
+          BrightnessControl.value = null;
+          DimDisplay.idleTime = 780000; # ​    13 min
+          DPMSControl.idleTime = 900; # ​      15 min
+          SuspendSession.idleTime = null;
+        };
+        Battery = {
+          BrightnessControl.value = null;
+          DimDisplay.idleTime = 480000; # ​     8 min
+          DPMSControl.idleTime = 600; # ​      10 min
+          SuspendSession.idleTime = 900000; # ​15 min
+        };
+        LowBattery = {
+          BrightnessControl.value = 30; # ​       30%
+          DimDisplay.idleTime = 180000; # ​     3 min
+          DPMSControl.idleTime = 300; # ​       5 min
+          SuspendSession.idleTime = 600000; # ​10 min
+        };
+      };
+
       # Use OpenGL 3 rendering backend.
       kwinrc.Compositing.GLCore = true;
 
