0b64d2cc4c443c00501c4d2f9a59f0682a23f417
Samuel Gräfenstein <git@samuelgrf.com>
2021-07-06 23:03:49 +0200

    kde/power-management: Hibernate on low battery

--- home/kde/power-management.nix
+++ home/kde/power-management.nix
@@ -3,21 +3,25 @@
   powermanagementprofilesrc = {
     AC = {
       BrightnessControl.value = null;
-      DimDisplay.idleTime = 780000; # ​    13 min
-      DPMSControl.idleTime = 900; # ​      15 min
+      DimDisplay.idleTime = 780000; # ​       13 min
+      DPMSControl.idleTime = 900; # ​         15 min
       SuspendSession.idleTime = null;
     };
     Battery = {
       BrightnessControl.value = null;
-      DimDisplay.idleTime = 480000; # ​     8 min
-      DPMSControl.idleTime = 600; # ​      10 min
-      SuspendSession.idleTime = 900000; # ​15 min
+      DimDisplay.idleTime = 480000; # ​        8 min
+      DPMSControl.idleTime = 600; # ​         10 min
+      SuspendSession.idleTime = 900000; # ​   15 min
     };
     LowBattery = {
-      BrightnessControl.value = 30; # ​       30%
-      DimDisplay.idleTime = 180000; # ​     3 min
-      DPMSControl.idleTime = 300; # ​       5 min
-      SuspendSession.idleTime = 600000; # ​10 min
+      BrightnessControl.value = 30; # ​          30%
+      DimDisplay.idleTime = 180000; # ​        3 min
+      DPMSControl.idleTime = 300; # ​          5 min
+      SuspendSession = {
+        idleTime = 600000; # ​                10 min
+        suspendType = 2; # ​               Hibernate
+      };
+      HandleButtonEvents.lidAction = 2; # ​Hibernate
     };
   };
 
