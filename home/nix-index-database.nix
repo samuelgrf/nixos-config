86ea7e0f3e91ceaa6139c7125b046a2824f14ee1
Samuel Gräfenstein <git@samuelgrf.com>
2021-07-25 09:03:49 +0200

    home/nix-index-database: 2021-07-18 -> 2021-07-25

--- home/nix-index-database.nix
+++ home/nix-index-database.nix
@@ -1,10 +1,10 @@
 { fetchurl, ... }: {
 
-  home.file.".cache/nix-index/files".source = let release = "2021-07-18";
+  home.file.".cache/nix-index/files".source = let release = "2021-07-25";
   in fetchurl {
     url =
       "https://github.com/Mic92/nix-index-database/releases/download/${release}/files";
-    hash = "sha256-xxkILxe2YcguDY2hkIqi2tqLoru+NxeF/gjcBws3mzY=";
+    hash = "sha256-5eEipHcr4N8uTbIb3pPeRZKuHi36y44eSbzi92NOYDk=";
   };
 
 }
