90772dc38352fd38fb9dac77b2c2f7280c306a8f
Samuel Gräfenstein <git@samuelgrf.com>
2021-07-27 22:32:23 +0200

    home/nix-index-database: Improve update script

--- home/nix-index-database/update.sh
+++ home/nix-index-database/update.sh
@@ -4,13 +4,18 @@ set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-API_DATA=$(curl https://api.github.com/repos/Mic92/nix-index-database/releases/latest)
+API_DATA=$(curl -sS https://api.github.com/repos/Mic92/nix-index-database/releases/latest)
 RELEASE_TAG=$(echo "$API_DATA" | jq -r .tag_name)
 
 DL_URL="https://github.com/Mic92/nix-index-database/releases/download/$RELEASE_TAG/files"
-HASH=$(nix-prefetch-url "$DL_URL")
+OLD_DL_URL=$(nix-instantiate --eval -E "(import $SCRIPT_DIR/source.nix).url" | xargs)
 
-echo "{
+if [ "$DL_URL" = "$OLD_DL_URL" ]; then
+  echo "Already up to date."
+else
+  HASH=$(nix-prefetch-url "$DL_URL")
+  echo "{
   url = \"$DL_URL\";
   sha256 = \"$HASH\";
 }" > "$SCRIPT_DIR/source.nix"
+fi
