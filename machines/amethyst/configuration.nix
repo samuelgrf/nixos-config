e64c65100844ede808d378db27c10c0d49b12b2a
Samuel Gräfenstein <git@samuelgrf.com>
2021-08-23 20:33:36 +0200

    amethyst: Fix sound crackling workaround

--- machines/amethyst/configuration.nix
+++ machines/amethyst/configuration.nix
@@ -45,6 +45,7 @@
   systemd.services.sound-crackling-workaround = {
     description = "Sound crackling workaround for Realtek ALC295";
     script = with binPaths; ''
+      until [ -e /dev/snd/hwC0D0 ]; do sleep 1; done
       ${hda-verb} /dev/snd/hwC0D0 0x20 SET_COEF_INDEX 0x67
       ${hda-verb} /dev/snd/hwC0D0 0x20 SET_PROC_COEF 0x3000
     '';
