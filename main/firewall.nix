b3631c03e78f3a104658302a24854b071ffc527d
Samuel Gräfenstein <git@samuelgrf.com>
2021-08-24 19:35:54 +0200

    firewall: Open ports for Minecraft server

--- main/firewall.nix
+++ main/firewall.nix
@@ -1,12 +1,21 @@
-{
+{ lib, ... }: {
 
-  # Steam Remote Play
-  networking.firewall = {
-    allowedTCPPorts = [ 27036 ];
-    allowedUDPPortRanges = [{
-      from = 27031;
-      to = 27036;
-    }];
-  };
+  networking.firewall = lib.mkMerge [
+
+    # Minecraft server
+    {
+      allowedTCPPorts = [ 25565 ];
+      allowedUDPPorts = [ 25565 ];
+    }
+
+    # Steam Remote Play
+    {
+      allowedTCPPorts = [ 27036 ];
+      allowedUDPPortRanges = [{
+        from = 27031;
+        to = 27036;
+      }];
+    }
+  ];
 
 }
