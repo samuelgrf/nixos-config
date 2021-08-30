a15844818cbf34f8e855bbc960e1afabcbd879c8
Samuel Gräfenstein <git@samuelgrf.com>
2021-08-30 21:22:59 +0200

    home/nix-index-database: Improve update script

--- home/nix-index-database/update.sh
+++ home/nix-index-database/update.sh
@@ -1,21 +1,21 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p curlMinimal jq
+
 set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SOURCE_FILE="$SCRIPT_DIR/source.nix"
+sourceAttr () { nix-instantiate --eval -E "(import \"$SOURCE_FILE\").$1" | xargs; }
 
 API_DATA=$(curl -sS https://api.github.com/repos/Mic92/nix-index-database/releases/latest)
-RELEASE_TAG=$(echo "$API_DATA" | jq -r .tag_name)
-
-DL_URL="https://github.com/Mic92/nix-index-database/releases/download/$RELEASE_TAG/files"
-OLD_DL_URL=$(nix-instantiate --eval -E "(import $SCRIPT_DIR/source.nix).url" | xargs)
+DL_URL=$(echo "$API_DATA" | jq -r '.assets[0].browser_download_url')
+OLD_DL_URL=$(sourceAttr url)
 
 if [ "$DL_URL" = "$OLD_DL_URL" ]; then
   echo "Already up to date."
 else
   HASH=$(nix-prefetch-url "$DL_URL")
-  echo "{
-  url = \"$DL_URL\";
-  sha256 = \"$HASH\";
-}" > "$SCRIPT_DIR/source.nix"
+  OLD_HASH=$(sourceAttr sha256)
+  sed -i "s,$OLD_DL_URL,$DL_URL," "$SOURCE_FILE"
+  sed -i "s,$OLD_HASH,$HASH," "$SOURCE_FILE"
 fi
