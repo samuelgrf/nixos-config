8cddee62352df5f3a5469c79af58f71e92ce34d7
Samuel Gräfenstein <git@samuelgrf.com>
2021-10-02 19:33:01 +0200

    overlays/update: Parallelize

--- overlays/update.sh
+++ overlays/update.sh
@@ -5,5 +5,6 @@ OVERLAY_DIR=$(dirname "$(readlink -f "$0")")
 FLAKE_DIR="$OVERLAY_DIR/.."
 
 cd "$FLAKE_DIR"
-"overlays/gimpPlugins/update.sh"
-"overlays/nix-index-database/update.sh"
+"overlays/gimpPlugins/update.sh" &
+"overlays/nix-index-database/update.sh" &
+wait
