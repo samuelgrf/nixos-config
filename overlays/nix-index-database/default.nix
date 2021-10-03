19ad54e0281cf06d808f38c9441c83aeebfd1efb
Samuel Gräfenstein <git@samuelgrf.com>
2021-10-03 12:56:36 +0200

    overlays/nix-index-database: 2021-09-26 -> 2021-10-03

--- overlays/nix-index-database/default.nix
+++ overlays/nix-index-database/default.nix
@@ -3,12 +3,12 @@ with prev; {
 
   nix-index-database = stdenv.mkDerivation rec {
     pname = "nix-index-database";
-    version = "2021-09-26";
+    version = "2021-10-03";
 
     src = fetchurl {
       url =
         "https://github.com/Mic92/nix-index-database/releases/download/${version}/index-x86_64-linux";
-      hash = "sha256-6tKjxFFLMSG/TutWN29fv6ezHoDfMn9blI4A7cvH5dE=";
+      hash = "sha256-E7IXunAvoW3T4FfmeOgjfvj+nZTRSd6tfgGzlK/5PTM=";
     };
 
     dontUnpack = true;
