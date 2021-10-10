ec52794546cad61a39c5112a7fb54203864e4f96
Samuel Gräfenstein <git@samuelgrf.com>
2021-10-10 21:03:44 +0200

    overlays/nix-index-database/update: Improve

--- overlays/nix-index-database/update.sh
+++ overlays/nix-index-database/update.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p common-updater-scripts gitMinimal jq
+#! nix-shell -i bash -p common-updater-scripts gitMinimal gnused jq
 set -euo pipefail
 
 ATTR=nix-index-database
 GIT_URL=https://github.com/Mic92/nix-index-database.git
 
-VERSION="unstable-$(list-git-tags "$GIT_URL" 2>/dev/null | tail -1)"
-CHANGES=$(update-source-version "$ATTR" "$VERSION" --print-changes)
+VERSION=$(list-git-tags "$GIT_URL" 2>/dev/null | tail -1)
+CHANGES=$(update-source-version "$ATTR" "unstable-$VERSION" --print-changes)
 
 if [ "$CHANGES" != "[]" ]; then
   FILES=$(echo "$CHANGES" | jq -r '.[].files[]')
-  OLD_VERSION=$(echo "$CHANGES" | jq -r '.[].oldVersion')
+  OLD_VERSION=$(echo "$CHANGES" | jq -r '.[].oldVersion' | sed 's/^unstable-//')
   git commit $FILES -m "overlays/$ATTR: $OLD_VERSION -> $VERSION"
 fi
