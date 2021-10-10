88838db0ccc86ffbbfa159b300f33da0737b0e13
Samuel Gräfenstein <git@samuelgrf.com>
2021-10-10 21:00:11 +0200

    overlays/gimpPlugins/update: Improve

--- overlays/gimpPlugins/update.sh
+++ overlays/gimpPlugins/update.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p common-updater-scripts gitMinimal jq
+#! nix-shell -i bash -p common-updater-scripts gitMinimal gnused jq
 set -euo pipefail
 
 ATTR=gimpPlugins.bimp
 GIT_URL=https://github.com/alessandrofrancesconi/gimp-plugin-bimp.git
 
-VERSION=$(list-git-tags "$GIT_URL" 2>/dev/null | tail -1 | sed -e 's/^v//')
+VERSION=$(list-git-tags "$GIT_URL" 2>/dev/null | tail -1 | sed 's/^v//')
 CHANGES=$(update-source-version "$ATTR" "$VERSION" --print-changes)
 
 if [ "$CHANGES" != "[]" ]; then
