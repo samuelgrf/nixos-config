9ec2e85dee57ab9e2494630aca0cf10516ef082a
Samuel Gräfenstein <git@samuelgrf.com>
2021-10-30 23:08:00 +0200

    home/misc: Fix Plasma locale settings

--- home/misc.nix
+++ home/misc.nix
@@ -1,11 +1,18 @@
 {
+  home.file = {
+
+    # Expose Nixpkgs config to Nix tools (requires `--impure` flag).
+    ".config/nixpkgs/config.nix".source = ../main/nixpkgs.nix;
+
+    # Force Plasma to use system locale settings.
+    ".config/plasma-localerc".text = "";
+    ".config/plasma-localerc".force = true;
+  };
+
   # Force overwrite symlinks that KDE replaces with new files.
   xdg = {
     configFile."fontconfig/conf.d/10-hm-fonts.conf".force = true;
     configFile."mimeapps.list".force = true;
     dataFile."applications/mimeapps.list".force = true;
   };
-
-  # Expose Nixpkgs config to Nix tools (requires `--impure` flag).
-  home.file.".config/nixpkgs/config.nix".source = ../main/nixpkgs.nix;
 }
