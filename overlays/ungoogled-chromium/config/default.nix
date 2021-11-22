f3cd7c44d34554b179a05d1c957ffbf20092f0e7
Samuel Gräfenstein <git@samuelgrf.com>
2021-11-22 15:58:49 +0100

    overlays/ungoogled-chromium: Refactor flags

--- overlays/ungoogled-chromium/config/default.nix
+++ overlays/ungoogled-chromium/config/default.nix
@@ -2,42 +2,41 @@ _: prev:
 with prev; {
 
   ungoogled-chromium = let
+
+    flags = lib.concatMapStrings (s: " --" + s);
+    enableFeatures = l: " --enable-features=${__concatStringsSep "," l}";
+    disableFeatures = l: " --disable-features=${__concatStringsSep "," l}";
+
     argsOverride = ungoogled-chromium.passthru.argsOverride or { } // {
-      commandLineArgs = toString [
+      commandLineArgs = flags [
 
-        # Ungoogled flags
-        "--disable-search-engine-collection"
-        "--extension-mime-request-handling=always-prompt-for-install"
-        "--hide-tab-close-buttons"
-        "--popups-to-tabs"
-        "--remove-grab-handle"
-        "--remove-tabsearch-button"
-        "--show-avatar-button=incognito-and-guest"
-
-        # Experimental features
-        "--enable-features=${
-          lib.concatStringsSep "," [
-            "BackForwardCache:enable_same_site/true"
-            "CanvasOopRasterization"
-            "OverlayScrollbar"
-            "RawDraw"
-            "TabHoverCardImages"
-            "VaapiVideoDecoder"
-          ]
-        }"
-
-        # Revert settings page redesign
-        "--disable-features=SettingsLandingPageRedesign"
+        # Ungoogled
+        "disable-search-engine-collection"
+        "extension-mime-request-handling=always-prompt-for-install"
+        "hide-tab-close-buttons"
+        "popups-to-tabs"
+        "remove-grab-handle"
+        "remove-tabsearch-button"
+        "show-avatar-button=incognito-and-guest"
 
         # Dark mode
-        "--force-dark-mode"
+        "force-dark-mode"
 
         # Performance
-        "--enable-gpu-rasterization"
-        "--enable-oop-rasterization"
-        "--enable-zero-copy"
-        "--ignore-gpu-blocklist"
-      ];
+        "enable-gpu-rasterization"
+        "enable-oop-rasterization"
+        "enable-zero-copy"
+        "ignore-gpu-blocklist"
+
+      ] + enableFeatures [
+        "BackForwardCache:enable_same_site/true"
+        "CanvasOopRasterization"
+        "OverlayScrollbar"
+        "RawDraw"
+        "TabHoverCardImages"
+        "VaapiVideoDecoder"
+
+      ] + disableFeatures [ "SettingsLandingPageRedesign" ];
     };
 
   in ungoogled-chromium.override argsOverride // {
