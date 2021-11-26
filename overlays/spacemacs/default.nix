ea04b21de5e5f26142f34af9f3cbdd2a851c2ae8
Samuel Gräfenstein <git@samuelgrf.com>
2021-11-26 19:22:52 +0100

    overlays/spacemacs: Refactor

--- overlays/spacemacs/default.nix
+++ overlays/spacemacs/default.nix
@@ -1,24 +1,20 @@
 _: prev: {
 
   spacemacs = prev.callPackage ({ stdenv, fetchFromGitHub }:
-    let
-      rev = "050221ee35a7dd3f73457a94b192199f57de24b2";
-      sha256 = "A4gRj0vfJmum+ESYg4ydH4stAPFDMdAH5sEkvanwGVw=";
 
-    in stdenv.mkDerivation {
+    stdenv.mkDerivation {
       pname = "spacemacs";
       version = "unstable-2021-11-20";
 
       src = fetchFromGitHub {
         owner = "syl20bnr";
         repo = "spacemacs";
-        inherit rev sha256;
+        rev = "050221ee35a7dd3f73457a94b192199f57de24b2";
+        sha256 = "A4gRj0vfJmum+ESYg4ydH4stAPFDMdAH5sEkvanwGVw=";
       };
 
       dontUnpack = true;
 
       installPhase = "cp -r $src $out";
-
-      passthru = { inherit rev sha256; };
     }) { };
 }
--- overlays/spacemacs/update.sh
+++ overlays/spacemacs/update.sh
@@ -2,8 +2,6 @@
 #! nix-shell -i bash -p curlMinimal gitMinimal jq nix-prefetch-github
 set -euo pipefail
 
-cd "$(dirname "$(realpath "$0")")"
-
 pkgAttr () {
   nix eval --impure --raw --expr '
     let flake = __getFlake (toString ../..);
@@ -11,24 +9,30 @@ pkgAttr () {
   '"$1"
 }
 
-OWNER="syl20bnr"
-REPO="spacemacs"
+cd "$(dirname "$(realpath "$0")")"
+FILE=$(realpath ./default.nix)
 
+OWNER=syl20bnr
+REPO=spacemacs
 API_DATA=$(curl -sS "https://api.github.com/repos/$OWNER/$REPO/commits/develop")
-REV=$(echo "$API_DATA" | jq -r .sha)
-DATE=$(echo "$API_DATA" | jq -r .commit.committer.date | head -c10)
 
-OLD_REV=$(pkgAttr rev)
+DATE=$(echo "$API_DATA" | jq -r .commit.committer.date | head -c10)
+REV=$(echo "$API_DATA" | jq -r .sha)
 OLD_DATE=$(pkgAttr version | tail -c10)
-OLD_HASH=$(pkgAttr sha256)
+OLD_REV=$(pkgAttr src.rev)
 
 if [ "$DATE" = "$OLD_DATE" ]; then
-    echo "Already up to date."
+  echo "Not updating version, already $OLD_DATE."
 else
-    HASH=$(nix-prefetch-github "$OWNER" "$REPO" --rev "$REV" | jq -r .sha256)
-    sed -i "s,$OLD_REV,$REV," ./default.nix
-    sed -i "s,$OLD_DATE,$DATE," ./default.nix
-    sed -i "s,$OLD_HASH,$HASH," ./default.nix
+  echo "Updating $OLD_DATE -> $DATE in '$FILE'..."
 
-    git commit ./default.nix -m "spacemacs: $OLD_DATE -> $DATE"
+  HASH=$(nix-prefetch-github "$OWNER" "$REPO" --rev "$REV" | jq -r .sha256)
+  OLD_HASH=$(pkgAttr src.outputHash)
+
+  sed -i "$FILE" \
+    -e "s $OLD_DATE $DATE " \
+    -e "s $OLD_REV $REV " \
+    -e "s $OLD_HASH $HASH "
+
+  git commit "$FILE" -m "spacemacs: $OLD_DATE -> $DATE"
 fi
