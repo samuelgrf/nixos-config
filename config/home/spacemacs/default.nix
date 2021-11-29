2f51b9039eb2821fbb62ff1bf1f7ce2726239aa9
Samuel Gräfenstein <git@samuelgrf.com>
2021-11-29 10:53:02 +0100

    Revert "home/spacemacs: Improve activation time"
    This reverts commit afd6f933f05c57be84cfffff850695699a17de22.

--- config/home/spacemacs/default.nix
+++ config/home/spacemacs/default.nix
@@ -1,16 +1,9 @@
-{ flakes, lib, ... }: {
+{ flakes, ... }: {
 
   home.file = {
     ".spacemacs".source = ./.spacemacs;
 
-  } // (with lib;
-    let
-      spacemacs = flakes.spacemacs;
-      spacemacsDirs = __readDir spacemacs;
-      mapDirs = name: _:
-        nameValuePair ".emacs.d/${name}" {
-          recursive = name == "private" || name == "layers";
-          source = "${spacemacs}/${name}";
-        };
-    in mapAttrs' mapDirs spacemacsDirs);
+    ".emacs.d".source = flakes.spacemacs;
+    ".emacs.d".recursive = true;
+  };
 }
