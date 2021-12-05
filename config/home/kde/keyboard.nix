538e078970801ad77af70fc5cb22ca1ee1b64b5b
Samuel Gräfenstein <git@samuelgrf.com>
2021-12-05 03:38:56 +0100

    home/kde/keyboard: Fix

--- config/home/kde/keyboard.nix
+++ config/home/kde/keyboard.nix
@@ -4,10 +4,10 @@ with nixosConfig.services.xserver; {
 
   # Apply X keyboard config to Plasma Wayland.
   kxkbrc.Layout = {
+    Use = true;
     LayoutList = layout;
     VariantList = xkbVariant;
     Model = xkbModel;
     Options = xkbOptions;
-    ResetOldOptions = true;
   };
 }
