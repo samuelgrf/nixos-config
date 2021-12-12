1a4f8d17f730f1732d877c35d051e830ba62fbb3
Samuel Gräfenstein <git@samuelgrf.com>
2021-12-12 17:10:11 +0100

    nixos/nix: Configure remote building

--- config/nixos/nix.nix
+++ config/nixos/nix.nix
@@ -6,11 +6,17 @@
     extraOptions = ''
       experimental-features = nix-command flakes
       flake-registry = /etc/nix/registry.json
+      builders-use-substitutes = true
     '';
 
     binaryCaches = [ "https://nix-community.cachix.org" ];
+    trustedBinaryCaches =
+      [ "ssh-ng://amethyst" "ssh://amethyst" "ssh-ng://beryl" "ssh://beryl" ];
+
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "amethyst:QyDJyzh9W0VNiYmi5k+jKnlsAzf5zyM79lDfFb6xKPc="
+      "beryl:F86Ng/AlmQvOroSienBev7tuhTvHGyDDac/OwIKt85k="
     ];
 
     sshServe = {
