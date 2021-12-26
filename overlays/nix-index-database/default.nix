ebb3f2c8c673b271462881765989731d302b9b6f
Samuel Gräfenstein <git@samuelgrf.com>
2021-12-26 09:46:12 +0100

    nix-index-database: 2021-12-19 -> 2021-12-26

--- overlays/nix-index-database/default.nix
+++ overlays/nix-index-database/default.nix
@@ -4,13 +4,13 @@ _: prev: {
 
     stdenv.mkDerivation rec {
       pname = "nix-index-database";
-      version = "unstable-2021-12-19";
+      version = "unstable-2021-12-26";
 
       src = fetchurl {
         url = "https://github.com/Mic92/nix-index-database/releases/download/${
             lib.removePrefix "unstable-" version
           }/index-x86_64-linux";
-        sha256 = "sha256-Jpyg2m5oVlqJq8aU61XcNP+YMuzrydhBzy+eCai/XOI=";
+        sha256 = "sha256-dlRW6iLG6OghbmxWDfHciDCPHcn/y7bs9XBY+o6GuP0=";
       };
 
       dontUnpack = true;
