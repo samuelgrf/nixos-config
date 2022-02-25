03cfda02eb5491745af5386f47873a8dc75f51b1
Samuel Gräfenstein <s@muel.gr>
2022-02-25 23:15:00 +0100

    home/kde/wallpaper: Set desktop to `Picture of the Day`

--- config/home/kde/wallpaper.nix
+++ config/home/kde/wallpaper.nix
@@ -1,8 +1,11 @@
 { pkgs }: {
 
-  # Desktop
-  "plasma-org.kde.plasma.desktop-appletsrc".Containments."1".Wallpaper."org.kde.image".General.Image =
-    pkgs.nixos-artwork.wallpapers.nineish-dark-gray.kdeFilePath;
+  # Desktop: Get picture of the day from Unsplash.
+  "plasma-org.kde.plasma.desktop-appletsrc".Containments."1".Wallpaper."org.kde.potd".General =
+    {
+      Provider = "unsplash";
+      FillMode = 2; # Scaled and cropped
+    };
 
   # Lockscreen
   kscreenlockerrc.Greeter.Wallpaper."org.kde.image".General.Image =
