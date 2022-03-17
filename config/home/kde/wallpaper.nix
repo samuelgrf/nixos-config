b4be88e777b9606d2ec62c844d95ff32ab9ea6f6
Samuel Gräfenstein <s@muel.gr>
2022-03-17 18:05:10 +0100

    home/kde: Get wallpapers from `Travel` category

--- config/home/kde/wallpaper.nix
+++ config/home/kde/wallpaper.nix
@@ -1,11 +1,15 @@
 { pkgs }: {
 
   # Desktop: Get picture of the day from Unsplash.
-  "plasma-org.kde.plasma.desktop-appletsrc".Containments."1".Wallpaper."org.kde.potd".General =
-    {
+  "plasma-org.kde.plasma.desktop-appletsrc".Containments."1" = {
+    wallpaperplugin = "org.kde.potd";
+
+    Wallpaper."org.kde.potd".General = {
       Provider = "unsplash";
+      Category = 1065428; # Travel
       FillMode = 2; # Scaled and cropped
     };
+  };
 
   # Lockscreen
   kscreenlockerrc.Greeter.Wallpaper."org.kde.image".General.Image =
