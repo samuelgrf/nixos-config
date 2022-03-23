894f4d795f18014d7994561e4ff275fa3c6ce253
Samuel Gräfenstein <s@muel.gr>
2022-03-23 09:43:14 +0100

    overlays/yt-dlp: Remove redundant build input

--- config/nixpkgs/overlays/yt-dlp/default.nix
+++ config/nixpkgs/overlays/yt-dlp/default.nix
@@ -1,7 +1,7 @@
 _: prev:
 with prev.python3Packages; {
 
-  yt-dlp = yt-dlp.overrideAttrs (old: rec {
+  yt-dlp = yt-dlp.overrideAttrs (_: rec {
     pname = "yt-dlp";
     version = "2022.3.8.2";
 
@@ -9,8 +9,6 @@ with prev.python3Packages; {
       inherit pname version;
       sha256 = "sha256-aFRleMGObOh0ULU3adXVt/WiPlIJeEl222x8y/eVSyE=";
     };
-
-    propagatedBuildInputs = old.propagatedBuildInputs ++ [ brotli ];
 
     name = "${python.libPrefix}-${pname}-${version}";
   });
