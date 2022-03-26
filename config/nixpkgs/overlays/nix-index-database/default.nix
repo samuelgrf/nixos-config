a850e7c8418c3a36e2987f7caa7bca9be51e6b04
Samuel Gräfenstein <s@muel.gr>
2022-03-26 07:21:49 +0100

    nix-index-database: 2022-02-27 -> 2022-03-20

--- config/nixpkgs/overlays/nix-index-database/default.nix
+++ config/nixpkgs/overlays/nix-index-database/default.nix
@@ -4,13 +4,13 @@ _final: prev: {
 
     stdenvNoCC.mkDerivation rec {
       pname = "nix-index-database";
-      version = "unstable-2022-02-27";
+      version = "unstable-2022-03-20";
 
       src = fetchurl {
         url = "https://github.com/Mic92/nix-index-database/releases/download/${
             lib.removePrefix "unstable-" version
           }/index-x86_64-linux";
-        sha256 = "sha256-k8KkfHArwvqlmSH3KVSnyQPoEOepc0gNrVNJrKZUu58=";
+        sha256 = "sha256-wnIPca+dmr6zeWvQ31uQohEdcrkTh8AIw7GSMIKafII=";
       };
 
       dontUnpack = true;
