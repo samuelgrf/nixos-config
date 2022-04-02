8934d12ca7ed5b7f1b42e21ca750ed326bbf7d8e
Samuel Gräfenstein <s@muel.gr>
2022-04-02 17:12:06 +0200

    machines/amethyst: Hopefully fix system lock-ups

--- config/machines/amethyst/nixos.nix
+++ config/machines/amethyst/nixos.nix
@@ -21,7 +21,21 @@
   services.xserver.autorun = true;
 
   # Enable TLP for improved battery life.
-  services.tlp.enable = true
+  services.tlp.enable = true;
+
+  # Hopefully fix system lock-ups by disabling power management for network controller.
+  # Found here: https://h30434.www3.hp.com/t5/Notebooks-Archive-Read-Only/HP-Spectre-15-BL012DX-Completely-Freezes-Momentarily/m-p/6201824/highlight/true#M3144753
+  services.tlp.settings = {
+    RUNTIME_PM_BLACKLIST = "02:00.0";
+    WIFI_PWR_ON_BAT = "off";
+  };
+
+  # How to find the ASPM register: https://wireless.wiki.kernel.org/en/users/documentation/aspm#how_to_find_the_link_control_register_on_a_pcie_device
+  systemd.services.RTL8821CE-disable-aspm = {
+    description = "Disable ASPM power management on RTL8821CE";
+    script = "${pkgs.pciutils}/bin/setpci -s 00:1d.1 0x50.B=0x40";
+    wantedBy = [ "multi-user.target" ];
+  }
 
   ;
   ##############################################################################
